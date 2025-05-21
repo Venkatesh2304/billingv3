f86c39b7cdac90c9240d1ff61660168bdab20f63
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2025-05-21 10:22:32 +0530

    s

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,6 +4,6 @@ threads = 5
 max_requests = 1000
 max_requests_jitter = 50
 
-accesslog = "~/logs/access.log"
-errorlog = "~/logs/error.log"
+accesslog = "/home/ubuntu/logs/access.log"
+errorlog = "/home/ubuntu/logs/error.log"
 loglevel = "info"
