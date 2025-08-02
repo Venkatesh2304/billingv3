1960ba69914d993143fe9b37f5bcc668694bd43c
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2025-08-02 17:00:30 +0530

    s

--- app/management/commands/load_barcodes.py
+++ app/management/commands/load_barcodes.py
@@ -5,17 +5,17 @@ import datetime
 from collections import defaultdict
 from custom.classes import IkeaDownloader
 
-maps = json.load(open("barcodes.json","r"))
+barcode_to_basepack = json.load(open("barcodes.json","r"))
 BarcodeMap.objects.all().delete()
 fromd = datetime.date(2025,4,1)
 tod = datetime.date.today()
 
 i = IkeaDownloader()
 df = i.stock_master()
-maps = dict(zip(df["Basepack Code"].values , df["Product Code"].values))
+basepack_to_sku = dict(zip(df["Basepack Code"].values , df["Product Code"].values))
 
-for k,v in maps.items() :
+for barcode,basepack in barcode_to_basepack.items() :
     BarcodeMap.objects.create(
-        barcode = k,
-        varient = v,
-        sku = maps.get(v,None))
+        barcode = barcode,
+        varient = basepack,
+        sku = basepack_to_sku.get(basepack,None))
