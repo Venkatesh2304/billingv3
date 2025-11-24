cc59f647a146ba37640f53511f15aa02822139ac
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2025-11-24 23:41:07 +0530

    s

--- app/management/commands/sync.py
+++ app/management/commands/sync.py
@@ -1,6 +1,5 @@
 from collections import defaultdict
 from app.sync import sync_reports
-#,"beat":None,"collection":None,"sales":None,"adjustment":None
-sync_reports(limits={"party":None}
+sync_reports(limits={"party":None,"beat":None,"collection":None,"sales":None,"adjustment":None}
                 ,min_days_to_sync=defaultdict(lambda : 15))
 
